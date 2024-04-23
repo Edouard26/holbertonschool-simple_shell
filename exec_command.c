78b1304a3021aba6b982ab61e8477d3dfea56bbb
Edouard26 <edouard.halimi@gmail.com>
2024-04-23 15:16:07 +0200

    remove execvp and change the code

--- exec_command.c
+++ exec_command.c
@@ -1,24 +1,46 @@
 #include "shell.h"
 
-/**
- * execute_command -  forks a child process to execute the command
- * @args: array that specify the command and its arguments
- */
-
 void execute_command(char *args[]) 
 {
-	pid_t pid = fork();
+	pid_t pid; 
+	char *path;
+	char *dir;
+	int status;
 
-	if (pid == -1) {
+	path = getenv("PATH");
+	pid = fork();
+	if (pid == -1) 
+	{
 		perror("fork");
 		exit(EXIT_FAILURE);
-	} else if (pid == 0) {
-		if (execvp(args[0], args) == -1) {
-			perror(args[0]);
-			exit(EXIT_FAILURE);
+	} 
+
+	else if (pid == 0) 
+	{
+		dir = strtok(path, ":");
+		while (dir != NULL) 
+		{
+
+
+			strcpy(path, dir);
+			strcat(path, "/");
+			strcat(path, args[0]);
+
+			if (access(path, X_OK) == 0) 
+			{
+
+				execve(path, args, NULL);
+				perror(args[0]);
+				_exit(EXIT_FAILURE);
+			}
+			dir = strtok(NULL, ":");
 		}
-	} else {
-		int status;
+
+		fprintf(stderr, "%s: command not found\n", args[0]);
+		_exit(EXIT_FAILURE);
+	} 
+	else 
+	{
 		waitpid(pid, &status, 0);
 	}
 }
