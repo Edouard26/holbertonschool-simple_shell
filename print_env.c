83a077949b9c5a89380a283627d2fed12ea8dd13
Edouard26 <edouard.halimi@gmail.com>
2024-04-24 11:58:59 +0200

    add _getenv

--- print_env.c
+++ print_env.c
@@ -15,3 +15,22 @@ void print_environment(void)
 		env++;
 	}
 }
+
+/**
+ *_getenv - get a variable environment
+ *@env: list of variables environment
+ *@name: name of get
+ *Return: Variable value of environment or NULL
+ */
+char *_getenv(char **env, const char *name) {
+	int i = 0;
+
+	while (env[i] != NULL) {
+		if (strncmp(env[i], name, strlen(name)) == 0 && (env[i])[strlen(name)] == '=') {
+			return (env[i] + strlen(name) + 1);
+		}
+		i++;
+	}
+	return (NULL);
+
+}
