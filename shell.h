82a0e0e72188cb52dbede2d75348240f95d34afc
Gniare <niare09@hotmail.fr>
2024-04-26 12:02:07 +0200

    AUTHORS

--- shell.h
+++ shell.h
@@ -1,15 +1,24 @@
+#ifndef SHELL_H
+#define SHELL_H
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
-#define MAX_COMMAND_LENGTH 100
-#define MAX_ARGS 20
-
-int command_exists(char *command);
-void print_environment();
-void execute_command(char **args);
-char *_getenv(char **env, const char *name);
 extern char **environ;
+char *read_line(void);
+int built_in(char **command);
+void handle_built_in(char **command, int *status);
+void hsh_exit(char **cmd, int *status);
+char *_path(char *command);
+char *get_env(char *var);
+char *_strdup(const char *s);
+char **_str(char *line);
+void free_array(char **command);
+int exec(char **arr, char **av, int idx);
+int main(__attribute__((unused)) int ac, char **av);
+void hsh_env(char **cmd, int *status);
+#endif
